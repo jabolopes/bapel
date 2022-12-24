79949a2b292f0f6a9c7da3ea798106319fafd2a4
Jose Lopes <jabolopes@gmail.com>
2022-12-24 23:47:54 +0100

    Extend ssa.

--- ssa/assemble.go
+++ ssa/assemble.go
@@ -44,6 +44,25 @@ func family(callback func(ir.IrType) error) func(*Context, []string) error {
 	}
 }
 
+func pushImmediateOrVar(context *Context, destType ir.IrType, token string) error {
+	if value, err := ir.ParseNumber[uint64](token); err == nil {
+		// Push immediate.
+		return context.assembler.PushImmediate(destType, value)
+	}
+
+	// Push variable.
+	sourceVar, ok := context.assembler.LookupVar(token)
+	if !ok {
+		return fmt.Errorf("Undefined variable %q", token)
+	}
+
+	if sourceVar.Type != destType {
+		return fmt.Errorf("Variable %q has type %d instead of %d", token, destType, sourceVar.Type)
+	}
+
+	return context.assembler.PushVar(token)
+}
+
 func assemblePush(context *Context, args []string) error {
 	if len(args) != 1 && len(args) != 2 {
 		return fmt.Errorf("expected 1 or 2 arguments; got %q", args)
@@ -98,59 +117,94 @@ func assembleDefineVar(typ ir.IrType) func(*Context, []string) error {
 	}
 }
 
-func assembleInstruction(context *Context, line string) error {
-	line = strings.TrimSpace(line)
-
-	if line == "" {
-		return nil
-	}
-
-	for _, instruction := range context.instructions {
-		if strings.HasPrefix(line, instruction.token) {
-			line = strings.TrimPrefix(line, instruction.token)
-			line = strings.TrimPrefix(line, " ")
-			var args []string
-			if line != "" {
-				args = strings.Split(line, " ")
-			}
-			return instruction.callback(context, args)
-		}
-	}
-
-	return fmt.Errorf("Unknown instruction line %q", line)
-}
-
+// assembleAssign2Args assembles an assign op where the right side is
+// either a variable or a literal. The token '<-' should not be passed
+// in 'args'.
+//
 // x <- y
 // x <- 123
 func assembleAssign2Args(context *Context, args []string) error {
 	dest := args[0]
 	source := args[1]
 
-	value, err := ir.ParseNumber[uint64](source)
-	if err == nil {
-		destVar, ok := context.assembler.LookupVar(dest)
-		if !ok {
-			return fmt.Errorf("Undefined variable %q", dest)
-		}
+	destVar, ok := context.assembler.LookupVar(dest)
+	if !ok {
+		return fmt.Errorf("Undefined variable %q", dest)
+	}
 
-		if err := context.assembler.PushImmediate(destVar.Type, value); err != nil {
-			return err
-		}
-	} else {
-		if err := context.assembler.PushVar(source); err != nil {
-			return err
-		}
+	if err := pushImmediateOrVar(context, destVar.Type, source); err != nil {
+		return err
 	}
 
 	return context.assembler.PopVar(dest)
 }
 
+// assembleAssign3Args assembles an assign op where the right side is
+// a unary op on either a variable or a literal. The token '<-' should
+// not be passed in 'args'.
+//
+// x <- <unaryOp> y
+// x <- <unaryOp> 123
 func assembleAssign3Args(context *Context, args []string) error {
-	return nil
+	dest := args[0]
+	op := args[1]
+	source := args[2]
+
+	destVar, ok := context.assembler.LookupVar(dest)
+	if !ok {
+		return fmt.Errorf("Undefined variable %q", dest)
+	}
+
+	if err := pushImmediateOrVar(context, destVar.Type, source); err != nil {
+		return err
+	}
+
+	switch op {
+	default:
+		return fmt.Errorf("Undefined op %q", op)
+	}
+
+	return context.assembler.PopVar(dest)
 }
 
+// assembleAssign3Args assembles an assign op where the right side is
+// a binary op on a pair of variables and/or literals. The token '<-'
+// should not be passed in 'args'.
+//
+// x <- y   <binaryOp> z
+// x <- 123 <binaryOp> 456
+// x <- y   <binaryOp> 123
+// x <- 123 <binaryOp> y
 func assembleAssign4Args(context *Context, args []string) error {
-	return nil
+	dest := args[0]
+	source1 := args[1]
+	op := args[2]
+	source2 := args[3]
+
+	destVar, ok := context.assembler.LookupVar(dest)
+	if !ok {
+		return fmt.Errorf("Undefined variable %q", dest)
+	}
+
+	if err := pushImmediateOrVar(context, destVar.Type, source1); err != nil {
+		return err
+	}
+
+	if err := pushImmediateOrVar(context, destVar.Type, source2); err != nil {
+		return err
+	}
+
+	switch op {
+	case "+":
+		if err := context.assembler.Add(destVar.Type); err != nil {
+			return err
+		}
+
+	default:
+		return fmt.Errorf("Undefined op %q", op)
+	}
+
+	return context.assembler.PopVar(dest)
 }
 
 func assembleAssign(context *Context, args []string) error {
@@ -172,6 +226,28 @@ func assembleFallback(context *Context, args []string) error {
 	}
 
 	return fmt.Errorf("Unknown instruction %q", args)
+}
+
+func assembleInstruction(context *Context, line string) error {
+	line = strings.TrimSpace(line)
+
+	if line == "" {
+		return nil
+	}
+
+	for _, instruction := range context.instructions {
+		if strings.HasPrefix(line, instruction.token) {
+			line = strings.TrimPrefix(line, instruction.token)
+			line = strings.TrimPrefix(line, " ")
+			var args []string
+			if line != "" {
+				args = strings.Split(line, " ")
+			}
+			return instruction.callback(context, args)
+		}
+	}
+
+	return fmt.Errorf("Unknown instruction line %q", line)
 }
 
 func assembleFile(context *Context, input *os.File) error {
