83f23bf0ccea0c240734682d21444b1c48e7d6cb
Jose Lopes <jabolopes@gmail.com>
2022-12-27 18:58:01 +0100

    Remove unnecessary calculations.

--- vm/tape.go
+++ vm/tape.go
@@ -12,28 +12,28 @@ type Tape struct {
 
 func (t Tape) GetI8() byte {
 	const size = 1
-	value := t.data[*t.pc : *t.pc+size]
+	value := t.data[*t.pc:]
 	*t.pc += size
 	return value[0]
 }
 
 func (t Tape) GetI16() uint16 {
 	const size = 2
-	value := binary.LittleEndian.Uint16(t.data[*t.pc : *t.pc+size])
+	value := binary.LittleEndian.Uint16(t.data[*t.pc:])
 	*t.pc += size
 	return value
 }
 
 func (t Tape) GetI32() uint32 {
 	const size = 4
-	value := binary.LittleEndian.Uint32(t.data[*t.pc : *t.pc+size])
+	value := binary.LittleEndian.Uint32(t.data[*t.pc:])
 	*t.pc += size
 	return value
 }
 
 func (t Tape) GetI64() uint64 {
 	const size = 8
-	value := binary.LittleEndian.Uint64(t.data[*t.pc : *t.pc+size])
+	value := binary.LittleEndian.Uint64(t.data[*t.pc:])
 	*t.pc += size
 	return value
 }
