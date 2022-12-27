213e26d1bff3660a5d93b044d392fd76bb54bc52
Jose Lopes <jabolopes@gmail.com>
2022-12-27 19:02:34 +0100

    More docs.

--- ir/ir_function.go
+++ ir/ir_function.go
@@ -70,7 +70,24 @@ func (f *IrFunction) addVar(id string, irvar IrVar) error {
 //
 // To make the return, the callee is responsible for deallocating (or
 // popping) the fp, the pc, the locals, and the args from the stack,
-// leaving only the rets. The rets are then managed by the caller.//
+// leaving only the rets. The rets are then managed by the caller.
+//
+// Locals size (enter size):
+//
+// The function needs to know the size in bytes for the locals to
+// allocate them on the stack.
+//
+// This size is not an operand to the 'call' opcode because that would
+// leak the implementation details of the function and therefore
+// whenever the function's frame size changed, all call sites would
+// have to be recompiled, which is not possible.
+//
+// There's also no 'enter' or 'leave' opcodes, which would mean
+// additional operations that every function had to call. Instead, the
+// first i16 word of the function's body is the size of the
+// locals. The 'call' opcode jumps to the function's pc and reads the
+// i16 word directly from the function's op data, which also advances
+// the pc to the first proper instruction of the function's body.
 //
 // PC handling:
 //
@@ -104,6 +121,13 @@ func (f *IrFunction) addVar(id string, irvar IrVar) error {
 //   localn  -- offset(localn-1) + sizeof(localn)
 //   local2  -- offset(local1)   + sizeof(local2)
 //   local1  -- sizeof(local1)
+//
+// Steps:
+// 1. Caller allocates (or pushes) rets.
+// 2. Caller allocates (or pushes) args.
+// 3. Caller invokes 'call' with the offset of the callee's function.
+// 4. Callee runs.
+// 5. Callee invokes 'return' with the frame size.
 func (f *IrFunction) computeFrame() error {
 	const pcSize = 8
 
