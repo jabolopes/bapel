f05cbe9deeffe98fac8260288ccea36490b2de1a
Jose Lopes <jabolopes@gmail.com>
2022-12-28 23:14:52 +0100

    Better filenames.

--- vm/gen.go
+++ vm/gen.go
@@ -1,4 +1,4 @@
 package vm
 
-//go:generate sh -c "m4 unaryops.m4 > unaryops.go"
-//go:generate sh -c "m4 binops.m4 > binops.go"
+//go:generate sh -c "m4 unary_ops.m4 > unary_ops.go"
+//go:generate sh -c "m4 binary_ops.m4 > binary_ops.go"
