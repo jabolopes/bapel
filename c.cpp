e35815a0675f389d8273ce16981ead650585d03c
Jose Lopes <jabolopes@gmail.com>
2023-08-20 01:39:27 +0200

    Add static pool.

--- c.cpp
+++ c.cpp
@@ -1,5 +1,6 @@
 export module c;
 
+import <array>;
 import <cerrno>;
 import <cstdint>;
 import <ctime>;
@@ -15,4 +16,21 @@ std::tuple<int64_t, int64_t> time() {
   return {res, 0};
 }
 
-}
+}  // namespace c
+
+export namespace ecs {
+
+template <typename K, typename V, int64_t Capacity>
+class StaticPool final {
+ public:
+  StaticPool() {}
+
+  V Get(K key) { return dense_.at(key); }
+
+  void Set(K key, V value) { dense_[key] = value; }
+
+ private:
+  std::array<V, Capacity> dense_;
+};
+
+}  // namespace entity
