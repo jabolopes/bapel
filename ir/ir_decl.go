434c603f334a70936ebe7768761975c0e178854b
Jose Lopes <jabolopes@gmail.com>
2023-09-02 23:08:19 +0200

    Add String() method to IrDecl.

--- ir/ir_decl.go
+++ ir/ir_decl.go
@@ -1,6 +1,9 @@
 package ir
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type IrDeclCase int
 
@@ -24,6 +27,25 @@ type IrDecl struct {
 	Case IrDeclCase
 	ID   string
 	Type IrType
+}
+
+func (d IrDecl) String() string {
+	switch d.Case {
+	case TypeDecl:
+		var b strings.Builder
+		b.WriteString(fmt.Sprintf("type %s : ", d.ID))
+		b.WriteString(d.Type.String())
+		return b.String()
+
+	case TermDecl:
+		var b strings.Builder
+		b.WriteString(fmt.Sprintf("%s : ", d.ID))
+		b.WriteString(d.Type.String())
+		return b.String()
+
+	default:
+		panic(fmt.Errorf("unhandled IrDeclCase %d", d.Case))
+	}
 }
 
 func NewTypeDecl(id string, typ IrType) IrDecl {
