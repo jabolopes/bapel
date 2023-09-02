56a65cda593e4ba2e9c5834506c46b20ff0892f6
Jose Lopes <jabolopes@gmail.com>
2023-09-02 23:08:01 +0200

    Add String() method to Sign.

--- ir/sign.go
+++ ir/sign.go
@@ -1,8 +1,21 @@
 package ir
 
+import "fmt"
+
 type Sign byte
 
 const (
 	Unsigned = Sign(iota)
 	Signed
 )
+
+func (s Sign) String() string {
+	switch s {
+	case Unsigned:
+		return "unsigned"
+	case Signed:
+		return "signed"
+	default:
+		panic(fmt.Errorf("unhandled Sign %d", s))
+	}
+}
