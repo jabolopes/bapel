72d186830e4fe141bc06917e42defc17a88d9222
Jose Lopes <jabolopes@gmail.com>
2024-04-14 23:41:56 +0200

    Implement type decls in parser.

--- bplparser/decl.go
+++ bplparser/decl.go
@@ -4,12 +4,33 @@ import (
 	"github.com/jabolopes/bapel/ir"
 )
 
-func (p *Parser) parseDeclImpl() (ir.IrDecl, error) {
-	isType := false
-	if p.shiftLiteral("type") == nil {
-		isType = true
+func (p *Parser) parseTypeDecl() (ir.IrDecl, error) {
+	if err := p.shiftLiteral("type"); err != nil {
+		return ir.IrDecl{}, err
 	}
 
+	typ, err := p.parseQuantifiedType()
+	if err != nil {
+		return ir.IrDecl{}, err
+	}
+
+	if p.shiftLiteral("=") == nil {
+		typ2, err := p.parseQuantifiedType()
+		if err != nil {
+			return ir.IrDecl{}, err
+		}
+
+		typ = ir.NewAliasType(typ, typ2)
+	}
+
+	if err := p.eol(); err != nil {
+		return ir.IrDecl{}, err
+	}
+
+	return ir.NewTypeDecl(typ), nil
+}
+
+func (p *Parser) parseTermDecl() (ir.IrDecl, error) {
 	id, err := p.shiftID()
 	if err != nil {
 		return ir.IrDecl{}, err
@@ -28,11 +49,15 @@ func (p *Parser) parseDeclImpl() (ir.IrDecl, error) {
 		return ir.IrDecl{}, err
 	}
 
-	if isType {
-		panic("not yet implemented")
+	return ir.NewTermDecl(id, typ), nil
+}
+
+func (p *Parser) parseDeclImpl() (ir.IrDecl, error) {
+	if p.peek("type") {
+		return p.parseTypeDecl()
 	}
 
-	return ir.NewTermDecl(id, typ), nil
+	return p.parseTermDecl()
 }
 
 func (p *Parser) parseDecl() (result ir.IrDecl, err error) {
