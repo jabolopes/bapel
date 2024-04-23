a42b1edff7eccb4d36c4898b5e7c5bed4a110b30
Jose Lopes <jabolopes@gmail.com>
2024-04-23 23:13:25 +0200

    Add tests for list value.

--- ts/list/list_test.go
+++ ts/list/list_test.go
@@ -22,22 +22,27 @@ func TestList(t *testing.T) {
 	tests := []struct {
 		input     list.List[int]
 		want      []int
+		wantValue int
 		wantEmpty bool
 		wantSize  int
 	}{
-		{l, []int{}, true, 0},
-		{l1, []int{1}, false, 1},
-		{l2, []int{1, 2}, false, 2},
-		{l3, []int{1, 2, 3}, false, 3},
-		{l4, []int{1, 2}, false, 2},
-		{l5, []int{1}, false, 1},
-		{l6, []int{}, true, 0},
-		{l7, []int{}, true, 0},
+		{l, []int{}, 0, true, 0},
+		{l1, []int{1}, 1, false, 1},
+		{l2, []int{1, 2}, 2, false, 2},
+		{l3, []int{1, 2, 3}, 3, false, 3},
+		{l4, []int{1, 2}, 2, false, 2},
+		{l5, []int{1}, 1, false, 1},
+		{l6, []int{}, 0, true, 0},
+		{l7, []int{}, 0, true, 0},
 	}
 
 	for _, test := range tests {
 		if got := test.input.Iterate().Collect(); !cmp.Equal(got, test.want, cmpopts.EquateEmpty()) {
 			t.Errorf("Iterate(%v).Collect() = %v; want %v", test.input, got, test.want)
+		}
+
+		if got, gotOk := test.input.Value(); got != test.wantValue || gotOk != !test.wantEmpty {
+			t.Errorf("Value(%v) = %v; want %v", test.input, got, test.wantValue)
 		}
 
 		if got := test.input.Empty(); got != test.wantEmpty {
