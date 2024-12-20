e50bb83daaf411445791d222c4efaab8cd9da5e2
Jose Lopes <jabolopes@gmail.com>
2024-12-21 00:30:45 +0100

    Delete old parser's rules.

--- bplparser/parser.go
+++ bplparser/parser.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/jabolopes/bapel/parser"
-	"golang.org/x/exp/constraints"
 )
 
 type Parser struct {
@@ -15,16 +14,6 @@ type Parser struct {
 	line    string
 	words   []string
 	lineNum int
-}
-
-func (p *Parser) withCheckpoint(callback func() error) error {
-	orig := p.words
-	if err := callback(); err != nil {
-		p.words = orig
-		return err
-	}
-
-	return nil
 }
 
 func (p *Parser) Open(reader io.Reader) {
@@ -58,7 +47,7 @@ func (p *Parser) Scan() bool {
 
 func (p *Parser) ScanErr() error {
 	if p.scanner == nil {
-		return fmt.Errorf("parser was not initialize; Open() must be called first")
+		return fmt.Errorf("must be initialized by calling Open() first")
 	}
 
 	return p.scanner.Err()
@@ -77,68 +66,6 @@ func (p *Parser) LineNum() int {
 }
 
 func (p *Parser) ShiftToken() (parser.Token, error) {
-	return p.shiftToken()
-}
-
-func (p *Parser) EOL() error {
-	return p.eol()
-}
-
-func NewParser() *Parser {
-	return &Parser{
-		nil, /* scanner */
-		"",  /* line */
-		nil, /* words */
-		0,   /* lineNum */
-	}
-}
-
-func (p *Parser) peek(token string) bool {
-	return len(p.words) > 0 && p.words[0] == token
-}
-
-func (p *Parser) getPeek() (string, bool) {
-	if len(p.words) <= 0 {
-		return "", false
-	}
-	return p.words[0], true
-}
-
-func (p *Parser) peekRune(match func(rune) bool) bool {
-	token, ok := p.getPeek()
-	if !ok {
-		return false
-	}
-
-	var r rune
-	for _, r = range token {
-		break
-	}
-
-	return match(r)
-}
-
-func (p *Parser) shiftID() (string, error) {
-	id, words, err := parser.ShiftID(p.words)
-	if err != nil {
-		return "", err
-	}
-
-	p.words = words
-	return id, nil
-}
-
-func (p *Parser) shiftLiteral(token string) error {
-	words, err := parser.ShiftLiteral(p.words, token)
-	if err != nil {
-		return err
-	}
-
-	p.words = words
-	return nil
-}
-
-func (p *Parser) shiftToken() (parser.Token, error) {
 	token, words, err := parser.ShiftToken(p.words)
 	if err != nil {
 		return parser.Token{}, err
@@ -148,38 +75,11 @@ func (p *Parser) shiftToken() (parser.Token, error) {
 	return token, nil
 }
 
-func (p *Parser) eol() error {
-	return parser.EOL(p.words)
-}
-
-func shiftInteger[T constraints.Integer](p *Parser) (T, error) {
-	integer, words, err := parser.ShiftNumber[T](p.words)
-	if err != nil {
-		var t T
-		return t, err
+func NewParser() *Parser {
+	return &Parser{
+		nil, /* scanner */
+		"",  /* line */
+		nil, /* words */
+		0,   /* lineNum */
 	}
-
-	p.words = words
-	return integer, nil
-}
-
-func ParseFile(input io.Reader) ([]Source, error) {
-	parser := NewParser()
-	parser.Open(input)
-
-	var sources []Source
-	for parser.Scan() {
-		source, err := parser.ParseAny()
-		if err != nil {
-			return nil, fmt.Errorf("in line %d:\n  %s\n%v", parser.LineNum(), parser.Line(), err)
-		}
-
-		sources = append(sources, source)
-	}
-
-	if err := parser.ScanErr(); err != nil {
-		return nil, err
-	}
-
-	return sources, nil
 }
--- parser/shift.go
+++ parser/shift.go
@@ -1,60 +1,8 @@
 package parser
 
 import (
-	"fmt"
 	"io"
-
-	"golang.org/x/exp/constraints"
 )
-
-func shiftIf[T comparable](args []T, token T, err error) ([]T, error) {
-	if len(args) == 0 || args[0] != token {
-		return args, err
-	}
-	return args[1:], nil
-}
-
-func shiftIfEnd[T comparable](args []T, token T, err error) ([]T, error) {
-	if len(args) == 0 || args[len(args)-1] != token {
-		return args, err
-	}
-	return args[:len(args)-1], nil
-}
-
-func shift[T any](args []T, err error) (T, []T, error) {
-	var t T
-	if len(args) == 0 {
-		return t, args, err
-	}
-	return args[0], args[1:], nil
-}
-
-func ShiftLiteral[T comparable](args []T, token T) ([]T, error) {
-	return shiftIf(args, token, fmt.Errorf("expected token '%v'; got %v", token, args))
-}
-
-func ShiftLiteralEnd[T comparable](args []T, token T) ([]T, error) {
-	return shiftIfEnd(args, token, fmt.Errorf("expected token '%v' at end of line; got %v", token, args))
-}
-
-func ShiftID[T any](args []T) (T, []T, error) {
-	return shift(args, fmt.Errorf("expected identifier; got %v", args))
-}
-
-func ShiftNumber[T constraints.Integer](args []string) (T, []string, error) {
-	var t T
-
-	if len(args) == 0 {
-		return t, args, io.EOF
-	}
-
-	number, err := parseNumber[T](args[0])
-	if err != nil {
-		return t, args, err
-	}
-
-	return number, args[1:], nil
-}
 
 func ShiftToken(args []string) (Token, []string, error) {
 	if len(args) == 0 {
@@ -67,12 +15,4 @@ func ShiftToken(args []string) (Token, []string, error) {
 	}
 
 	return token, args[1:], nil
-}
-
-func EOL[T any](args []T) error {
-	if len(args) > 0 {
-		return fmt.Errorf("expected end of line; got %v", args)
-	}
-
-	return nil
 }
