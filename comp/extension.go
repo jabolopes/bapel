24c2362114f6a00489ab04125f44bc75631b98fa
Jose Lopes <jabolopes@gmail.com>
2025-01-13 23:11:36 +0100

    Add functions to manipulate extensions.

---
+++ comp/extension.go
@@ -0,0 +1,19 @@
+package comp
+
+import (
+	"fmt"
+	"path"
+	"strings"
+)
+
+func TrimExtension(filename string) string {
+	if ext := path.Ext(filename); len(ext) > 0 {
+		filename = strings.TrimSuffix(filename, ext)
+		filename = strings.TrimSuffix(filename, ".")
+	}
+	return filename
+}
+
+func ReplaceExtension(filename, replacement string) string {
+	return fmt.Sprintf("%s%s", TrimExtension(filename), replacement)
+}
