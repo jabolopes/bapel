2252c8f0327dbcbc1674434c174bb8c9c42021b1
Jose Lopes <jabolopes@gmail.com>
2025-06-25 20:25:31 +0200

    Add script that diffs the output directory based on a flag.

---
+++ dflag.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+flag=$1
+
+rm -rf /tmp/out1
+rm -rf /tmp/out2
+
+go run ./bin -alsologtostderr --${1}=false build program.bpl
+cp -r out /tmp/out1
+
+go run ./bin -alsologtostderr --${1}=true build program.bpl
+cp -r out /tmp/out2
+
+meld /tmp/out1 /tmp/out2
