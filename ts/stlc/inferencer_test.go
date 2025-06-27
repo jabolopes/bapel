741ee6e370c9fbbe7e643d77ce0076d5cb61c5a2
Jose Lopes <jabolopes@gmail.com>
2025-06-27 23:32:57 +0200

    Add typechecker tests.

--- ts/stlc/inferencer_test.go
+++ ts/stlc/inferencer_test.go
@@ -1,6 +1,7 @@
 package stlc_test
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -20,60 +21,81 @@ func init() {
 	flag.BoolVar(&regen, "regen", false, "Whether to regenerate test output files.")
 }
 
-func TestInferTerm(t *testing.T) {
-	matches, err := filepath.Glob("inferencer_test_*.in")
+func globTests(pattern string) ([]string, error) {
+	matches, err := filepath.Glob(pattern)
 	if err != nil {
-		t.Fatal(err)
+		return nil, err
 	}
 
 	if len(matches) == 0 {
-		t.Fatal("Found no tests")
+		return nil, errors.New("Found no tests")
+	}
+
+	return matches, nil
+}
+
+func checkModule(filename string, typecheck bool) (ast.Module, error) {
+	context := stlc.NewContext()
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return ast.Module{}, err
+	}
+	defer file.Close()
+
+	module, err := bplparser2.ParseFile(file.Name(), file)
+	if err != nil {
+		return ast.Module{}, err
+	}
+
+	for i := range module.Body {
+		source := &module.Body[i]
+
+		switch source.Case {
+		case ast.DeclSource:
+			context, err = context.AddSymbol(source.Decl.Decl, stlc.DeclSymbol)
+			if err != nil {
+				return ast.Module{}, err
+			}
+
+		case ast.FunctionSource:
+			typechecker := stlc.NewTypechecker(context)
+
+			if !typecheck {
+				var err error
+				context, err = typechecker.InferFunction(source.Function)
+				if err != nil {
+					return ast.Module{}, err
+				}
+			} else {
+				var err error
+				if _, err = typechecker.InferFunction(source.Function); err != nil {
+					return ast.Module{}, err
+				}
+
+				context, err = typechecker.TypecheckFunction(source.Function)
+				if err != nil {
+					return ast.Module{}, err
+				}
+			}
+		}
+	}
+
+	return module, nil
+}
+
+func TestInferTerm(t *testing.T) {
+	matches, err := globTests("inferencer_test_*.in")
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	for _, inFile := range matches {
 		wantFile := fmt.Sprintf("%s.out", strings.TrimSuffix(inFile, ".in"))
 
-		context := stlc.NewContext()
-
-		in, err := os.Open(inFile)
-		if os.IsNotExist(err) {
-			break
-		}
+		module, err := checkModule(inFile, false /* typecheck */)
 		if err != nil {
 			t.Fatalf("in test %s: %v", inFile, err)
-		}
-		defer in.Close()
-
-		module, err := bplparser2.ParseFile(in.Name(), in)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		// TODO: Deduplicate with compiler.
-		//
-		// TODO: Finish.
-		for i, source := range module.Body {
-			switch source.Case {
-			case ast.ComponentSource:
-				t.Fatalf("in test %s: ComponentSource is not yet supported", inFile)
-
-			case ast.DeclSource:
-				context, err = context.AddSymbol(source.Decl.Decl, stlc.DeclSymbol)
-				if err != nil {
-					t.Fatalf("in test %s: %v", inFile, err)
-				}
-
-			case ast.FunctionSource:
-				typechecker := stlc.NewTypechecker(context)
-
-				var err error
-				context, err = typechecker.InferFunction(source.Function)
-				if err != nil {
-					t.Fatalf("in test %s: %v", inFile, err)
-				}
-
-				module.Body[i] = source
-			}
 		}
 
 		got := fmt.Sprintf("%+s\n", module)
@@ -91,6 +113,19 @@ func TestInferTerm(t *testing.T) {
 
 		if diff := cmp.Diff(string(want), got); len(diff) > 0 {
 			t.Errorf("Infer() diff = %s", diff)
+		}
+	}
+}
+
+func TestTypecheckTerm(t *testing.T) {
+	matches, err := globTests("inferencer_test_*.in")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, inFile := range matches {
+		if _, err := checkModule(inFile, true /* typecheck */); err != nil {
+			t.Fatalf("in test %s: %v", inFile, err)
 		}
 	}
 }
