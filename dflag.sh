119cb3fba071f72241bc234021e2f42c275eeecb
Jose Lopes <jabolopes@gmail.com>
2025-07-02 21:49:57 +0200

    Bring back errors but don't fail the bpl command.

--- dflag.sh
+++ dflag.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 flag=$1
 
 rm -rf /tmp/out1
 rm -rf /tmp/out2
 
-go run ./bin -alsologtostderr --${1}=false build program.bpl
+go run ./bin -alsologtostderr --${1}=false build program.bpl || true
 cp -r out /tmp/out1
 
-go run ./bin -alsologtostderr --${1}=true build program.bpl
+go run ./bin -alsologtostderr --${1}=true build program.bpl || true
 cp -r out /tmp/out2
 
 meld /tmp/out1 /tmp/out2
