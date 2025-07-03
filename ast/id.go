a378d845757e16f184652ff8f6b76da8204d99cb
Jose Lopes <jabolopes@gmail.com>
2025-07-03 20:32:15 +0200

    Fix ID formatting.
    Add ID validation.

--- ast/id.go
+++ ast/id.go
@@ -17,13 +17,29 @@ func (i ID) Format(f fmt.State, verb rune) {
 	if addMetadata := f.Flag('+'); addMetadata {
 		i.Pos.Format(f, verb)
 	}
+
+	isQuoted := verb == 'q'
+	if isQuoted {
+		fmt.Fprint(f, `"`)
+	}
 	fmt.Fprint(f, i.Value)
+	if isQuoted {
+		fmt.Fprint(f, `"`)
+	}
+}
+
+func NewID(value string, pos ir.Pos) ID {
+	return ID{value, pos}
+}
+
+func ValidateID(id ID) error {
+	if len(id.Value) == 0 {
+		return fmt.Errorf("invalid ID %q. IDs cannot be empty", id)
+	}
+
+	return nil
 }
 
 func CompareID(id1, id2 ID) int {
 	return cmp.Compare(id1.Value, id2.Value)
-}
-
-func NewID(name string, pos ir.Pos) ID {
-	return ID{name, pos}
 }
