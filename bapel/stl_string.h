50a9c1a5a93eb97cac6aa2b150d32f9ddb714dbf
Jose Lopes <jabolopes@google.com>
2026-06-03 08:01:57 +0000

    Extend STL string.

--- bapel/stl_string.h
+++ bapel/stl_string.h
@@ -9,14 +9,25 @@
 using String = std::string;
 using StringView = std::string_view;
 
+// @bpl: pub to_string: forall ['a] 'a -> String
+template <typename T>
+inline String to_string(T value) { return std::to_string(value); }
+
+
 // @bpl: pub String_::empty: String -> bool
 // @bpl: pub String_::front: String -> i8
 // @bpl: pub String_::size: String -> i64
+// @bpl: pub String_::view: String -> StringView
+// @bpl: pub String_::from_view: StringView -> String
+// @bpl: pub String_::from_char: i8 -> String
 namespace String_ {
 
 inline bool empty(String s) { return s.empty(); }
 inline char front(String s) { return s.front(); }
 inline int64_t size(String s) { return s.size(); }
+inline StringView view(const String& s) { return s; }
+inline String from_view(StringView v) { return String(v); }
+inline String from_char(char c) { return String(1, c); }
 
 }  // namespace String_
 
