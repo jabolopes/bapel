6ae5eb73ce1d114dad81d093369315d05859db36
Jose Lopes <jabolopes@google.com>
2026-06-03 08:01:05 +0000

    Extend STL deque.

--- bapel/stl_deque.h
+++ bapel/stl_deque.h
@@ -11,7 +11,10 @@ template <typename A>
 using Deque = std::deque<A>;
 
 // @bpl: pub Deque_::mk: forall ['a] () -> Deque 'a
-// @bpl: pub Deque_::push_back: forall ['a] (&Deque 'a, 'a) -> ()
+// @bpl: pub Deque_::push_back: forall ['a] (& (Deque 'a), 'a) -> ()
+// @bpl: pub Deque_::pop_front: forall ['a] & (Deque 'a) -> ()
+// @bpl: pub Deque_::front: forall ['a] & (Deque 'a) -> 'a
+// @bpl: pub Deque_::empty: forall ['a] & (Deque 'a) -> bool
 namespace Deque_ {
 
 template <typename A>
@@ -21,6 +24,22 @@ template <typename A>
 std::monostate push_back(Deque<A>* deque, A a) {
 	deque->push_back(std::move(a));
 	return std::monostate();
+}
+
+template <typename A>
+std::monostate pop_front(Deque<A>* deque) {
+	deque->pop_front();
+	return std::monostate();
+}
+
+template <typename A>
+A front(Deque<A>* deque) {
+	return deque->front();
+}
+
+template <typename A>
+bool empty(Deque<A>* deque) {
+	return deque->empty();
 }
 
 }  // namespace Deque_
